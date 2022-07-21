a203cb2a5b37a79e7fa31415c971846887d621e6
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2022-07-21 19:47:44 -0400

    FULLY WORKING PASSWORD MANAGE VERSION  0.1 NEEDS IMPROVEMENTS AND QUALITY OF LIFE

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,50 +29,25 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Index));
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.ProgressBar = new System.Windows.Forms.ToolStripProgressBar();
             this.ProgressLabel = new System.Windows.Forms.ToolStripStatusLabel();
             this.Input1 = new System.Windows.Forms.TextBox();
             this.AutoCompleteSuggestionBox = new System.Windows.Forms.CheckedListBox();
-            this.AccountInfoDisplay = new System.Windows.Forms.CheckedListBox();
             this.creationDateLabel = new System.Windows.Forms.Label();
             this.commentLabel = new System.Windows.Forms.Label();
             this.lastUsedLabel = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Checked = true;
-            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox1.Location = new System.Drawing.Point(12, 12);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(101, 19);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "Has username";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Checked = true;
-            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBox2.Location = new System.Drawing.Point(115, 12);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(99, 19);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "Has Password";
-            this.checkBox2.UseVisualStyleBackColor = true;
             // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.ProgressBar,
             this.ProgressLabel});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 406);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 276);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(352, 22);
             this.statusStrip1.TabIndex = 2;
@@ -80,12 +55,16 @@
             // 
             // ProgressBar
             // 
+            this.ProgressBar.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.ProgressBar.Size = new System.Drawing.Size(100, 16);
             // 
             // ProgressLabel
             // 
+            this.ProgressLabel.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ProgressLabel.Name = "ProgressLabel";
+            this.ProgressLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.ProgressLabel.Size = new System.Drawing.Size(38, 17);
             this.ProgressLabel.Text = "0/null";
             // 
@@ -96,11 +75,11 @@
             this.Input1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
             this.Input1.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.Input1.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.Input1.Location = new System.Drawing.Point(13, 37);
+            this.Input1.Location = new System.Drawing.Point(12, 12);
             this.Input1.MaxLength = 45;
             this.Input1.Name = "Input1";
             this.Input1.PlaceholderText = "Input a title";
-            this.Input1.Size = new System.Drawing.Size(327, 26);
+            this.Input1.Size = new System.Drawing.Size(328, 26);
             this.Input1.TabIndex = 4;
             this.Input1.Tag = "InputType";
             this.Input1.WordWrap = false;
@@ -113,76 +92,92 @@
             this.AutoCompleteSuggestionBox.CheckOnClick = true;
             this.AutoCompleteSuggestionBox.Cursor = System.Windows.Forms.Cursors.Hand;
             this.AutoCompleteSuggestionBox.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.AutoCompleteSuggestionBox.Location = new System.Drawing.Point(13, 69);
+            this.AutoCompleteSuggestionBox.Location = new System.Drawing.Point(12, 44);
             this.AutoCompleteSuggestionBox.Name = "AutoCompleteSuggestionBox";
-            this.AutoCompleteSuggestionBox.Size = new System.Drawing.Size(327, 137);
+            this.AutoCompleteSuggestionBox.Size = new System.Drawing.Size(328, 137);
             this.AutoCompleteSuggestionBox.TabIndex = 2;
             this.AutoCompleteSuggestionBox.Tag = "InputType";
             this.AutoCompleteSuggestionBox.Visible = false;
             this.AutoCompleteSuggestionBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.AutoCompleteSuggestionBox_ItemCheck);
             this.AutoCompleteSuggestionBox.SelectedIndexChanged += new System.EventHandler(this.AutoCompleteSuggestionBox_SelectedIndexChanged);
             // 
-            // AccountInfoDisplay
-            // 
-            this.AccountInfoDisplay.BackColor = System.Drawing.Color.Gainsboro;
-            this.AccountInfoDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.AccountInfoDisplay.CheckOnClick = true;
-            this.AccountInfoDisplay.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.AccountInfoDisplay.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.AccountInfoDisplay.Location = new System.Drawing.Point(13, 287);
-            this.AccountInfoDisplay.Name = "AccountInfoDisplay";
-            this.AccountInfoDisplay.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.AccountInfoDisplay.Size = new System.Drawing.Size(327, 107);
-            this.AccountInfoDisplay.TabIndex = 5;
-            this.AccountInfoDisplay.Tag = "InputType";
-            this.AccountInfoDisplay.SelectedIndexChanged += new System.EventHandler(this.AccountInfoDisplay_SelectedIndexChanged);
-            // 
             // creationDateLabel
             // 
             this.creationDateLabel.AutoSize = true;
             this.creationDateLabel.BackColor = System.Drawing.Color.Gainsboro;
-            this.creationDateLabel.Location = new System.Drawing.Point(13, 209);
+            this.creationDateLabel.Location = new System.Drawing.Point(12, 184);
             this.creationDateLabel.Name = "creationDateLabel";
             this.creationDateLabel.Size = new System.Drawing.Size(82, 15);
             this.creationDateLabel.TabIndex = 6;
             this.creationDateLabel.Text = "Creation Date:";
+            this.creationDateLabel.Visible = false;
+            this.creationDateLabel.Click += new System.EventHandler(this.creationDateLabel_Click);
             // 
             // commentLabel
             // 
             this.commentLabel.AutoSize = true;
             this.commentLabel.BackColor = System.Drawing.Color.Gainsboro;
-            this.commentLabel.Location = new System.Drawing.Point(13, 269);
+            this.commentLabel.Location = new System.Drawing.Point(12, 214);
             this.commentLabel.Name = "commentLabel";
             this.commentLabel.Size = new System.Drawing.Size(69, 15);
             this.commentLabel.TabIndex = 7;
             this.commentLabel.Text = "Comments:";
+            this.commentLabel.Visible = false;
+            this.commentLabel.Click += new System.EventHandler(this.commentLabel_Click);
             // 
             // lastUsedLabel
             // 
             this.lastUsedLabel.AutoSize = true;
             this.lastUsedLabel.BackColor = System.Drawing.Color.Gainsboro;
-            this.lastUsedLabel.Location = new System.Drawing.Point(13, 239);
+            this.lastUsedLabel.Location = new System.Drawing.Point(12, 199);
             this.lastUsedLabel.Name = "lastUsedLabel";
             this.lastUsedLabel.Size = new System.Drawing.Size(60, 15);
             this.lastUsedLabel.TabIndex = 8;
             this.lastUsedLabel.Text = "Last Used:";
+            this.lastUsedLabel.Visible = false;
+            this.lastUsedLabel.Click += new System.EventHandler(this.lastUsedLabel_Click);
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.BackColor = System.Drawing.Color.Gainsboro;
+            this.usernameLabel.Location = new System.Drawing.Point(12, 238);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(63, 15);
+            this.usernameLabel.TabIndex = 9;
+            this.usernameLabel.Text = "Username:";
+            this.usernameLabel.Visible = false;
+            this.usernameLabel.Click += new System.EventHandler(this.usernameLabel_Click);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.BackColor = System.Drawing.Color.Gainsboro;
+            this.passwordLabel.Location = new System.Drawing.Point(12, 253);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(60, 15);
+            this.passwordLabel.TabIndex = 10;
+            this.passwordLabel.Text = "Password:";
+            this.passwordLabel.Visible = false;
+            this.passwordLabel.Click += new System.EventHandler(this.passwordLabel_Click);
             // 
             // Index
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(352, 428);
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(352, 298);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
             this.Controls.Add(this.lastUsedLabel);
             this.Controls.Add(this.commentLabel);
             this.Controls.Add(this.creationDateLabel);
-            this.Controls.Add(this.AccountInfoDisplay);
             this.Controls.Add(this.AutoCompleteSuggestionBox);
             this.Controls.Add(this.Input1);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Index";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Local Password Manager";
             this.statusStrip1.ResumeLayout(false);
@@ -193,17 +188,15 @@
         }
 
         #endregion
-
-        private CheckBox checkBox1;
-        private CheckBox checkBox2;
         private StatusStrip statusStrip1;
         private ToolStripProgressBar ProgressBar;
         private ToolStripStatusLabel ProgressLabel;
         private TextBox Input1;
         private CheckedListBox AutoCompleteSuggestionBox;
-        private CheckedListBox AccountInfoDisplay;
         private Label creationDateLabel;
         private Label commentLabel;
         private Label lastUsedLabel;
+        private Label usernameLabel;
+        private Label passwordLabel;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,6 @@
 namespace Local_Password_Manager
 #pragma warning disable CS8619 // Nullability of reference types in value doesn't match target type.
-
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
 {
     using Newtonsoft.Json.Linq;
 
@@ -21,6 +21,11 @@ namespace Local_Password_Manager
             ProgressBar.Value = 0;
             ProgressLabel.Text = "0/null";
             AutoCompleteSuggestionBox.Items.Clear();
+            creationDateLabel.Visible = false;
+            lastUsedLabel.Visible = false;
+            commentLabel.Visible = false;
+            usernameLabel.Visible = false;  
+            passwordLabel.Visible = false;
         }
 
         private void Input1_KeyUp(object sender, KeyEventArgs e)
@@ -98,17 +103,16 @@ namespace Local_Password_Manager
             return results;
         }
 
-        private void AccountInfoDisplay_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void AutoCompleteSuggestionBox_SelectedIndexChanged(object sender, EventArgs e)
+        public void AutoCompleteSuggestionBox_SelectedIndexChanged(object sender, EventArgs e)
         {
 
 
             string selectedKey = AutoCompleteSuggestionBox.SelectedItem.ToString();
-            clearAuto();
+            creationDateLabel.Visible = true;
+            lastUsedLabel.Visible = true;
+            commentLabel.Visible = true;
+            usernameLabel.Visible = true;
+            passwordLabel.Visible = true;   
             Input1.Text = "";
             AutoCompleteSuggestionBox.Visible = false;
 
@@ -121,11 +125,71 @@ namespace Local_Password_Manager
                     allNames.Add(title.Key.ToString());
                 }
 
-                creationDateLabel.Text = names[selectedKey]["creation-date"].ToString();
-                lastUsedLabel.Text = names[selectedKey]["last-used-date"].ToString();
 
 
+                creationDateLabel.Text = $@"Creation Date:  {names[selectedKey]["creation-date"].ToString()}";
+                lastUsedLabel.Text = $@"Last Used Date:  {names[selectedKey]["last-used-date"].ToString()}";
+                commentLabel.Text = $@"comments:    {names[selectedKey]["comments"].ToString()}";
+
+                usernameLabel.Text = $@"Username:   {names[selectedKey]["username"].ToString()}";
+
+                string pass = "";
+                for (int i = 0; i < names[selectedKey]["password"].ToString().Count(); i++)
+                {
+                    if (i < names[selectedKey]["password"].ToString().Count()/10)
+                    {
+                        pass = pass + names[selectedKey]["password"].ToString()[i];
+                    }
+                    else
+                    {
+                        pass = pass + "*";
+                    }
+                }
+
+                passwordLabel.Text = $@"Password:   {pass}";
             }
         }
+
+        private void updateLabels(string KEY)
+        {
+            string selectedKey = AutoCompleteSuggestionBox.SelectedItem.ToString();
+            using (StreamReader r = new StreamReader("1ab2ba2.json"))
+            {
+                JObject names = JObject.Parse(r.ReadToEnd());
+                List<string> allNames = new List<string>();
+                foreach (KeyValuePair<string, JToken> title in names)
+                {
+                    allNames.Add(title.Key.ToString());
+                }
+                Clipboard.SetText(names[selectedKey][KEY].ToString());
+            }
+            
+        }
+
+        private void creationDateLabel_Click(object sender, EventArgs e)
+        {
+            updateLabels("creation-date");
+        }
+
+        private void lastUsedLabel_Click(object sender, EventArgs e)
+        {
+            updateLabels("last-used-date");
+        }
+
+        private void commentLabel_Click(object sender, EventArgs e)
+        {
+            updateLabels("comments");
+        }
+
+        private void usernameLabel_Click(object sender, EventArgs e)
+        {
+            updateLabels("username");
+        }
+
+        private void passwordLabel_Click(object sender, EventArgs e)
+        {
+            updateLabels("password");
+        }
+
     }
 }
